261c5d1443f9280db86a6ed13567003344cedd2b
willieneis <willie.neiswanger@gmail.com>
2020-11-05 02:31:48 -0800

    add examples script

---
+++ examples/example_00.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import plot_python
+
+
+x = np.linspace(0, 10, 100)
+y = np.cos(x)
+z = np.sin(x) + 0.1 * x
+zz = -0.5 * np.cos(x)
+
+
+h1, = plt.plot(x, y, label='Cos($x$)')
+h2, = plt.plot(x, z, '--', label='Sin($x$) + $0.1x$')
+h3, = plt.plot(x, zz, ':', label='$-\\frac{1}{2}$Cos($x$)')
+
+
+plt.legend(handles=[h1, h2, h3], loc=1)
+
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+
+plt.xlim([-0.5, 10.5])
+plt.ylim([-1.5, 2.5])
+
+plt.title('Sine($x$) and Cosine($x$)')
+
+plot_python.save_figure('example')
+
+plt.show()
