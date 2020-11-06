42806d2b4af2f1e6a70ebc2fb51a9e169b39274f
willieneis <willie.neiswanger@gmail.com>
2020-11-06 00:50:32 -0800

    update save_figure to allow for single string ext_list

--- neatplot/__init__.py
+++ neatplot/__init__.py
@@ -9,6 +9,10 @@ plt.style.use((pathlib.Path(__file__).parent / 'matplotlibrc').resolve())
 
 def save_figure(file_name='figure', ext_list = ['pdf', 'png']):
     """Save figure for all extensions in ext_list."""
+
+    if isinstance(ext_list, str):
+        ext_list = [ext_list]
+
     for ext in ext_list:
         save_str = file_name + '.' + ext
         plt.savefig(save_str, bbox_inches='tight')
