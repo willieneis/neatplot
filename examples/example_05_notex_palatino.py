0172ecfc0f1481c681c215fa5dee98d771d2126e
Willie Neiswanger <willie.neiswanger@gmail.com>
2024-10-12 23:33:08 -0700

    Fix pyproject, add examples (#2)
    * minor fix to pyproject.toml fields formatting
    
    * add example_04_nolatex_sansserif.py
    
    * update neatplot argument for clarity
    
    * rename exampels to example_02_notex, example_03_fonts
    
    * rename and minor edits to example_04_notex_helvetica
    
    * add example_05_notex_palatino

---
+++ examples/example_05_notex_palatino.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import neatplot
+neatplot.set_style('notex')
+neatplot.update_rc('font.family', 'Palatino')
+
+
+# Data
+x = np.linspace(0, 10, 100)
+y = np.cos(x)
+z = np.sin(x) + 0.1 * x
+zz = -0.5 * np.cos(x)
+
+# Plot
+fig, ax = plt.subplots()
+ax.plot(x, y, label='Cos($x$)')
+ax.plot(x, z, '--', label='Sin($x$) + $0.1x$')
+ax.plot(x, zz, ':', label='$-\\frac{1}{2}$Cos($x$)')
+
+# Text
+ax.legend(loc=1)
+ax.set_xlabel('$x$')
+ax.set_ylabel('$y$')
+ax.set_title('Sine($x$) and Cosine($x$)')
+
+# Lims
+ax.set_xlim(-0.5, 10.5)
+ax.set_ylim(-1.5, 2.5)
+
+# Show figure
+neatplot.save_figure('05', 'png', fig)
+plt.show()
